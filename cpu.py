001951de4b3b9528ab5b0e48c3fd363c356a82c4
StoveBacon <keithstoffeljr@gmail.com>
2022-04-04 13:37:58 -0400

    Yeeted types, they were wasting too much time

--- cpu.py
+++ cpu.py
@@ -2,17 +2,14 @@ from functools import partial
 from operator import setitem
 from typing import Final, Tuple
 
-from byteops import uint8, uint16, unsigned_16
 from memory import Memory
 
-ZERO_MASK: Final[uint8] = 0x80
-SUBN_MASK: Final[uint8] = 0x40
-HALFC_MASK: Final[uint8] = 0x20
-CARRY_MASK: Final[uint8] = 0x10
+ZERO_MASK = 0x80
+SUBN_MASK = 0x40
+HALFC_MASK = 0x20
+CARRY_MASK = 0x10
 
-reglst = Final[list[str]]
-
-reg_ids: reglst = [
+reg_ids = [
     "B",
     "C",
     "D",
@@ -22,35 +19,35 @@ reg_ids: reglst = [
     "A"
 ]
 
-nonsplit_addr_reg_ids: reglst = [
+nonsplit_addr_reg_ids = [
     "PC",
     "SP"
 ]
 
-split_addr_reg_ids: reglst = [
+split_addr_reg_ids = [
     "BC",
     "DE",
     "HL"
 ]
 
-all_reg_ids: reglst = reg_ids + nonsplit_addr_reg_ids + split_addr_reg_ids
+all_reg_ids = reg_ids + nonsplit_addr_reg_ids + split_addr_reg_ids
 
 # All registers that can have their contents directly read or set.
-direct_access_reg_ids: reglst = reg_ids + nonsplit_addr_reg_ids
+direct_access_reg_ids = reg_ids + nonsplit_addr_reg_ids
 
 # All 16 bit registers. Can be used to read any memory address
-all_addr_reg_ids: reglst = nonsplit_addr_reg_ids + split_addr_reg_ids
+all_addr_reg_ids = nonsplit_addr_reg_ids + split_addr_reg_ids
 
 
 class CPU:
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.reg = self.dmg_8bit_registers()
         self.mem = Memory()
-        self.cycles: int = 0
+        self.cycles = 0
 
     # Register setup methods
-    def dmg_8bit_registers(self) -> dict[str, uint8]:
+    def dmg_8bit_registers(self):
         """Returns the 8-bit registers and their default values for the DMG."""
         return {
             "PC": 0x0100,
@@ -66,16 +63,16 @@ class CPU:
         }
 
     # Flag accessor methods
-    def zero(self) -> uint8:
+    def zero(self):
         return self.reg["F"] & ZERO_MASK
 
-    def subn(self) -> uint8:
+    def subn(self):
         return self.reg["F"] & SUBN_MASK
 
-    def halfc(self) -> uint8:
+    def halfc(self):
         return self.reg["F"] & HALFC_MASK
 
-    def carry(self) -> uint8:
+    def carry(self):
         return self.reg["F"] & CARRY_MASK
 
     def make_parameter_table(self):
--- gb_types.py
+++
@@ -1,5 +0,0 @@
-# Basic type aliases to help with type checking.
-# NewType had too much overhead and was too bulky in general.
-
-uint8 = int
-uint16 = int
